c07b6484f3363dd7da44032cdce3c6b53e378d1a
bofanh <bofanh3@gmail.com>
2022-05-15 00:39:59 -0400

    update

--- app/app.R
+++ app/app.R
@@ -1,49 +1,61 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# DTSC 630 - M01/Spring 2022
+# Data Visualization
+# HW-05
+# May 15, 2022
+# Bofan He
+# Dr. Cheng
 
-library(shiny)
+
+
+##############################################################################
+library(shiny) 
+library(ggplot2)
+
+##############################################################################
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- navbarPage(
+    title = "HOMEWORK 5",
+    
+    tabPanel( "Main",
+              fluidPage(
+                  checkboxGroupInput("drv", "Choose type(s) of drv:",
+                                     c("4 Wheels Drive" = "4",
+                                       "Rear Drive" = "r",
+                                       "Front Drive" = "f"),
+                                     selected = c("4 Wheels Drive" = "4",
+                                                  "Rear Drive" = "r",
+                                                  "Front Drive" = "f")
+                                     ),
+                  plotOutput("plot1")
+              )
+    ),
+    tabPanel(
+        "About",
+        p("DTSC 630 - M01/Spring 2022"),
+        p("Data Visualization"),
+        p("HW-05"),
+        p("May 15, 2022"),
+        p("Bofan He"),
+        p("Dr. Cheng"),
+        hr(),
+        tags$iframe(style = "height:1000px; width:100%", src =
+                        "DTSC630_HW5.pdf"), #testing pdf view
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$plot1 <- renderPlot({
+        ggplot(mpg[mpg$drv == input$drv, ], aes(displ, hwy, colour = factor(drv))) +
+            geom_point() +
+            geom_smooth(se = FALSE) +
+            labs(color="drv")+
+            xlim(NA, 7)
+           
     })
-}
+    }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
